1b9f712e4e166c71671edc00927fb0e82d856bac
NHeroOP <neeravtheheroo@gmail.com>
2024-03-10 13:49:09 +0530

    Add Layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import {store} from "./redux/store.js"
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<App />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
