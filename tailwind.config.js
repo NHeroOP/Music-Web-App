0c29ffcbd20077bc3b2c6a02ccb4c86807cbe728
NHeroOP <neeravtheheroo@gmail.com>
2024-03-10 13:44:31 +0530

    Add custom colors and font in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins": ["poppins", "sans-serif"]
+      },
+      colors: {
+        rare: "#BE5C2B",
+        secondary: "#FBFD82",
+        accent: "#232323",
+        bg: "#F2F2F2",
+      }
+    },
   },
   plugins: [],
 }
